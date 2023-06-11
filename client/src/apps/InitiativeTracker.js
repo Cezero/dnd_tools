813b0285b06766acd86277095baeb4075ddda919
Cezero <countzero@cyberdeck.org>
2023-06-11 11:53:16 -0400

    cleanout old project files for fresh start

--- client/src/apps/InitiativeTracker.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function InitiativeTracker() {
-    return <div className="InitiatveTracker">Initiative Tracker</div>;
-};
-
-export default InitiativeTracker;
--- server/dice/routes.js
+++
@@ -1,6 +0,0 @@
-const controllers = require('./controllers');
-const router = require('express').Router();
-
-router.post('/roll', controllers.rollDice);
-
-module.exports = router;
